07b46e3228fca93adfa63bdbf071cdad9a75b9be
Syed Ahmed <syedahmed3009@gmail.com>
2021-04-03 01:10:53 +0530

    project

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -1,10 +1,7 @@
-import React from 'react';
-
 export default function LoadingBox() {
-    return(
-        <div>
-            <i className="fa fa-spiner fa-spin">Loading ...</i>
-        </div>
-    )
-    
-}
+    return (
+      <div className="loading">
+        <i className="fa fa-spinner fa-spin"></i> Loading...
+      </div>
+    );
+  }
