6c282193c4fa3c8d279328df19f01358749bbef0
Syed Ahmed <syedahmed3009@gmail.com>
2021-04-06 16:22:10 +0530

    project

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,12 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/bestbuy', {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
